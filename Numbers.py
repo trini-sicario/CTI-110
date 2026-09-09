bbc50d5f6b7384151d4e2ed99a561a834f31159b
trini-sicario <223481850+trini-sicario@users.noreply.github.com>
2026-09-09 11:48:47 -0400

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,17 @@
+# Shammel Williams
+# 9 September 2026
+# math
+
+# hard code an integer
+Age = 43
+
+future_age = Age + 10
+print("In 10 years, you will be", future_age, "years old.")
+
+# hard code a float
+Money = 100.50
+
+Bonus = 5000
+
+total = Money + Bonus
+print(total-Age)
